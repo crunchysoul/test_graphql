9bb17134d009216a1fb0fcf61ca2da439a58690a
crunchysoul <zzzcca12@gmail.com>
2018-08-24 20:50:53 +1000

    add mutation

---
+++ lib/test_graphql_web/resolvers/news_resolver.ex
@@ -0,0 +1,18 @@
+defmodule TestGraphqlWeb.Resolvers.NewsResolver do
+  alias TestGraphql.News
+
+  def all_links(_root, _args, _info) do
+    links = News.list_links()
+    {:ok, links}
+  end
+
+  def create_link(_root, args, _info) do
+    case News.create_link(args) do
+      {:ok, link} ->
+        {:ok, link}
+
+      _error ->
+        {:error, "could not create link"}
+    end
+  end
+end
